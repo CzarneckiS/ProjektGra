54454572098c04c14f6f4ddfd8c7966e6295c3b4
mkornova <garyrootish@gmail.com>
2025-10-26 17:49:48 +0100

    bary

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -36,11 +36,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
 		if event.is_released():
 			cursor_move_animation()
-<<<<<<< Updated upstream
-  elif event.is_action_pressed("EscMenu"):
-=======
+
+
 	elif event.is_action_pressed("EscMenu"):
->>>>>>> Stashed changes
 		get_tree().change_scene_to_file("res://scenes/ui/esc_menu.tscn")
 
 func cursor_move_animation() -> void:
