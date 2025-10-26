97ba68cde8fd0abced725f657f70e1efbd6542b2
CzarneckiS <czarneckisebastian@op.pl>
2025-10-26 23:47:03 +0100

    Masywny refactoring
    Komentarze
    Poprawa nestowanych ifów
    Usuwanie bezużytecznego kodu

--- scenes/state_machine.gd
+++ scenes/state_machine.gd
@@ -1,40 +1,44 @@
 extends Node
 class_name StateMachine
 
-var state = null
-var previous_state = null
-#no fucking clue czemu to jest dictionary a nie array zwykly
-var states = {}
+#State - stan, w którym znajduje się jednostka (np przemieszcza się, atakuje coś)
+#State Machine to system, który przypisuje jednostkom odpowiedni state,
+#i sprawia, że jednostki wykonują rozkazy przypisane do danego state
+#state logic to zestaw rozkazów, które im wydajemy w każdym physics frame (np idź do celu)
+#get transition to zbiór warunków, które sprawdzają kiedy jednostka ma zmienić aktualny state
+#na przykład jeśli jesteś moving i dojdziesz do celu przejdź w state idle
+
+var state = null #aktualny stan
+var previous_state = null #Tymczasowo niezbyt ważne ale niech zostanie
+var states = {} #nasz dictionary wszystkich dostępnych stanów
 
 @onready var parent = get_parent()
 
 func _physics_process(delta: float) -> void:
 	if state != null:
-		_state_logic(delta)
-		_get_transition(delta)
+		_state_logic(delta) #Co jednostka ma robić
+		_get_transition(delta) #Warunki przejścia do nowego stanu
 
-#TU BEZ PODLOGI CHYBA BO KORZYSTAMY W JEGO DZIECIACH Z DELTY ALE NIE WIEM CZY MUSIMY W NICH KORZYSTAC
-#i guess w przyszlosci sie zobaczy, tak po prostu bylo w tutorialu
-func _state_logic(delta):
+func _state_logic(_delta):
 	pass
 
 func _get_transition(_delta):
 	pass
 
+#Zmień aktualny state w inny
 func set_state(new_state):
-	previous_state = state
-	state = new_state
+	previous_state = state #Tymczasowo niezbyt ważne ale niech zostanie
+	state = new_state #Przejdź w nowy stan
 	
-	if previous_state != null:
+	if previous_state != null:#Tymczasowo niezbyt ważne ale niech zostanie
 		_exit_state(previous_state, new_state)
 	if new_state != null:
 		_enter_state(new_state, previous_state)
-		
+
 func _exit_state(_previous_state, _new_state):
 	pass
-	
 func _enter_state(_new_state, _previous_state):
 	pass
 
-func add_state(state_name):
+func add_state(state_name): #dodawanie stateów do listy
 	states[state_name] = states.size()
--- scenes/warrior_state_machine.gd
+++ scenes/warrior_state_machine.gd
@@ -1,6 +1,7 @@
 extends StateMachine
 
 func _ready():
+	#dodawanie naszych stanów do słownika
 	add_state("idle")
 	add_state("moving")
 	add_state("engaging")
@@ -11,25 +12,30 @@ func _ready():
 	#kiedy juz inne states sie ladnie dodadza do list i wglaaa
 	call_deferred("set_state", states.idle)
 	#trzeba bedzie dodac kolejny stan - returning; kiedy jednostka odejdzie za daleko od glownej postaci
-	#to niewazne co robila wraca do nas
+	#to niewazne co robila, wraca do nas
 
-#co jednostka robi - sprawdzane w kazdym physics frame
+#Rozkazy dla jednostki wykonywane w physics_process
 func _state_logic(delta):
-		match state:
+		match state: #sprawdź w którym stanie teraz jesteś
 			states.idle:
-				pass
+				pass #jeśli jesteś idle to nic nie robisz
 			states.moving:
-				parent.move_to_target(delta, parent.move_target)
+				parent.move_to_target(delta, parent.move_target) #idź do celu (nie przeciwnik)
 			states.engaging:
-				if parent.attack_target.get_ref():
+				if parent.attack_target.get_ref(): #jeśli cel (jednostka) istnieje, idź do niego
 					parent.move_to_target(delta, parent.attack_target.get_ref().global_position)
 			states.attacking:
-				$"../AnimationPlayer".play("attack")
+				$"../AnimationPlayer".play("attack") #Jeśli zaczniesz atakować, zagraj animacje ataku
 			states.dying:
 				pass
 			states.mid_animation:
 				pass
 
+#Akcje wykonywane jednorazowo przy zmianie stanu PRZED state_logic
+#DO POPRAWY ! - nie działają poprawnie
+#enter state jest wykonywane PRZED state logic więc pytamy o nasz cel zeby zmienic kierunek
+#spritow zanim w ogóle go znajdziemy - bad news
+#Tylko dying jest poprawnym state
 func _enter_state(_new_state, _previous_state):
 		match state:
 			states.idle:
@@ -55,52 +61,50 @@ func _enter_state(_new_state, _previous_state):
 			states.attacking:
 				pass
 			states.dying:
-				$"../AnimationPlayer".play("dying")
+				$"../AnimationPlayer".play("dying") #Kiedy wejdziesz w state, rozpocznij animację
 			states.mid_animation:
 				pass
 
-func _get_transition(delta):
+#Warunki przejścia do nowego stanu - wykonywane PO state_logic
+func _get_transition(_delta):
 		match state:
-			states.idle:
-				if parent.closest_enemy() != null:
-					parent.attack_target = weakref(parent.closest_enemy())
-					set_state(states.engaging)
-			states.moving:
+			states.idle: #kiedy nic nie robisz
+				if parent.closest_enemy() != null: #jeśli jest jakiś przeciwnik w wizji
+					parent.attack_target = weakref(parent.closest_enemy()) #obierz go za cel
+					set_state(states.engaging) #zacznij do niego iść
+			states.moving: #przemieszczanie się do celu na ziemi
 				#jesli jednostka dojdzie do celu (a bardziej znajdzie sie w odleglosci mniejszej
 				#niz jakas tam wartosc stop distance) to sie zatrzyma i zacznie idlowac
 				if parent.global_position.distance_to(parent.move_target) < parent.stop_distance:
 					parent.move_target = parent.global_position
 					set_state(states.idle)
-			states.engaging:
-				#Jesli nasz cel znajdzie sie w naszym zasiegu ataku zacznij atakowac
+			states.engaging: #przemieszczanie się w stronę przeciwnika
 				#parent.speed = 600 #do debugowania rightclicka, cos na wysokim speedzie sie wali i jednoski przenikaja przez inne?
-				if parent.attack_target_within_attack_range() != null:
-					parent.attack_target = weakref(parent.closest_enemy())
+				if parent.attack_target_within_attack_range() != null: #Jesli cel znajdzie sie w zasiegu ataku
+					parent.attack_target = weakref(parent.closest_enemy()) #obierz go za cel
 					#parent.speed = 300 #debug
-					set_state(states.attacking)
-				if !parent.attack_target.get_ref():
+					set_state(states.attacking) #zacznij atakować
+				if !parent.attack_target.get_ref(): #jeśli nie masz celu
 					#parent.speed = 300 #debug
-					set_state(states.idle)
+					set_state(states.idle) #zacznij idlować
 			states.attacking:
+				#jeśli uda ci się zacząć atak przejdź w stan wykonywania animacji
 				if $"../AnimationPlayer".get_current_animation() == "attack":
 					set_state(states.mid_animation)
-				#elif parent.closest_enemy_within_attack_range() == null:
-					#set_state(states.engaging)
-			states.dying:
+			states.dying: #Dopóki odgrywasz animację umierania, nic nie rób
 				if $"../AnimationPlayer".is_playing(): return
-				else:
+				else: #kiedy się skończy, przestań istnieć
 					parent.queue_free()
-			states.mid_animation:
+			states.mid_animation: #Dopóki odgrywasz animację atakowania, nic nie rób
 				if $"../AnimationPlayer".is_playing(): return 
-				else:
-					#set_state(states.engaging)
-					#!sprawdzic czy attack target jest w zasiegu
-					if parent.closest_enemy_within_attack_range() != null:
-						parent.attack_target = weakref(parent.closest_enemy())
-						set_state(states.attacking)
+				else: #kiedy skończysz sprawdź czy cel wciąż jest w zasięgu ataku
+					if parent.attack_target_within_attack_range() != null:
+						set_state(states.attacking) #jeśli jest to go atakuj
 					else:
-						set_state(states.engaging)
+						set_state(states.engaging) #jeśli nie to go goń
+						#jeśli cel umarł to w stanie engaging to sprawdzi i przejdzie do idle
 
+#temporary do movementu
 func _on_move_timer_timeout() -> void:
 	if parent.get_slide_collision_count():
 		if abs(parent.last_position.distance_to(parent.move_target)) < \
