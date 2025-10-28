e3abb4597b09bcba686fdf28c22d2d05aa6378b9
CzarneckiS <czarneckisebastian@op.pl>
2025-10-28 15:00:44 +0100

    Revert "Merge branch 'main' into Cleanup-2"
    This reverts commit 8d41d005d5bc4932198ce6d79b60fd7d1aca15b5, reversing
    changes made to 88144cdf6917fec52ea7c338257050a3d1eb8aae.

--- scenes/characters/unit.gd
+++ scenes/characters/unit.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 class_name UnitParent
 
-
-var max_health 
-var health 
-var bar_style = StyleBoxFlat.new()
-
+#Klasa abstrakcyjna do wszystkich jednostek
+var health
 func _process(_delta: float) -> void:
 	pass
 func hit(_damage):
