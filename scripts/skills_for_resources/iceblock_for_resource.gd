ced980ca258a65600304b353b1fef464bef02469
CzarneckiS <czarneckisebastian@op.pl>
2025-12-14 20:06:24 +0100

    hotfix2

--- scripts/skills_for_resources/iceblock_for_resource.gd
+++ scripts/skills_for_resources/iceblock_for_resource.gd
@@ -14,3 +14,6 @@ func use(player: CharacterBody2D, target_position: Vector2) -> void:
 	var projectile_node = visual_effect.instantiate()
 	player.get_tree().root.add_child(projectile_node)
 	projectile_node.initialize(target_position, self)
+func _init() -> void:
+	unit_tags.append(Tags.UnitTag.PLAYER)
+	use_tags.append(Tags.UseTag.ACTIVE)
