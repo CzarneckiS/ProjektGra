7e252e726c29473cddfa80277cc2bd1af94ba466
CzarneckiS <czarneckisebastian@op.pl>
2026-01-09 17:33:54 +0100

    skill fix i dodane rarity skilli

--- scenes/globals/skills.gd
+++ scenes/globals/skills.gd
@@ -3,10 +3,12 @@ extends Node
 var all_skills: Array = [] #Wszystkie skille ktore mozemy zdobyc w trakcie runa (achievement odblokowany)
 var unlocked_skills: Array = [] #Wszystkie skille ktore wybralismy przy level upie
 
-
 var fireball = preload("res://resources/fireball.tres")
 var heal = preload("res://resources/heal.tres")
 var thunderbolt = preload("res://resources/thunderbolt.tres")
+var icewall = preload("res://resources/iceblock.tres")
+var blackhole = preload("res://resources/field.tres")
+var tornado = preload("res://resources/tornado.tres")
 var unit_on_hit_bleed = preload("res://resources/unit_on_hit_bleed.tres")
 var unit_on_hit_lifesteal = preload("res://resources/unit_on_hit_lifesteal.tres")
 var unit_stat_up_attack_up = preload("res://resources/unit_stat_up_attack_up.tres")
@@ -17,6 +19,26 @@ var player_skeleton_mage = preload("res://resources/player_skeleton_mage.tres")
 var player_summon_respawn_time_up = preload("res://resources/player_summon_respawn_time_up.tres")
 var player_life_on_kill = preload("res://resources/player_life_on_kill.tres")
 
+var legendary: float = 0.15
+var rare: float = 0.35
+var common: float = 0.5
+var skill_rarity_table : Dictionary = {
+	fireball: common,
+	heal: common,
+	thunderbolt: rare,
+	icewall: rare,
+	blackhole: legendary,
+	tornado: legendary,
+	unit_on_hit_bleed: rare,
+	unit_on_hit_lifesteal: rare,
+	unit_stat_up_attack_up: rare,
+	unit_stat_up_health_up: rare,
+	unit_unholy_frenzy: rare,
+	player_skeleton_warrior: common,
+	player_skeleton_mage: common,
+	player_summon_respawn_time_up: rare,
+	player_life_on_kill: rare
+}
 
 func add_skill(skill):
 	if skill not in all_skills:
@@ -37,13 +59,26 @@ func reset_unlocked_skills():
 
 #DODAC BOOL = CZY OSIAGNELISMY LIMIT SPELL SLOTOW ! ! ! 
 func get_skill() -> Array:
-	var available_skills: Array = []
+	var total_weight = calculate_total_weight() #sprawdzamy laczna wage skilli
+	var available_skills: Dictionary = {}
 	var skills_to_show: Array = []
-	for skill in all_skills:
-		available_skills.append(skill)
-	available_skills.shuffle()
-	for i in available_skills.size():
-		if i >=3:
-			break
-		skills_to_show.append(available_skills[i])
+	for skill in all_skills: #normalizujemy wage skilli (suma jest rowna 1)
+		available_skills[skill] = skill_rarity_table[skill] / total_weight
+	while skills_to_show.size() < 3:
+		var random_float = randf_range(0,1) #wybieramy numerek od 1 do 0
+		for skill in available_skills: #i przechodzimy kolejno po skillach w loot table
+			if random_float > available_skills[skill]: #i porownujemy czy numerek odpowiada skillowi
+				random_float -= available_skills[skill] #jak jest za duzy to odejmujemy wage skilla
+			else: #i przechodzimy dalej
+				if skill in skills_to_show: #sprawdzamy czy wylosowalismy powtorke
+					break
+				else:
+					skills_to_show.append(skill) #az numerek jest mniejszy niz waga czyli doszlismy do skilla wylosowanego
+					break
 	return skills_to_show
+
+func calculate_total_weight() -> float:
+	var total_weight = 0
+	for skill in all_skills:
+		total_weight += skill_rarity_table[skill]
+	return total_weight
--- scripts/skills_for_resources/tornado_for_resource.gd
+++ scripts/skills_for_resources/tornado_for_resource.gd
@@ -14,8 +14,13 @@ class_name Tornado
 @export var effect_heal: EffectHeal
 @export var amount: int
 @export var orb_spawn_frequency: int
-
+@export var unit_tags : PackedInt32Array = []
+@export var use_tags : PackedInt32Array = []
+@export var skill_tags : PackedInt32Array = []
 func use(player, target_position: Vector2) -> void:
 	var projectile_node = visual_effect.instantiate()
 	player.get_parent().add_child(projectile_node)
 	projectile_node.initialize(target_position, self)
+func _init() -> void:
+	unit_tags.append(Tags.UnitTag.PLAYER)
+	use_tags.append(Tags.UseTag.ACTIVE)
