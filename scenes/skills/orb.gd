f25c99c2dcabb430d9e8d672204f369f184b9824
vaoru <vaoru7@gmail.com>
2026-01-17 12:03:23 +0100

    orb pickup sfx

--- scenes/skills/orb.gd
+++ scenes/skills/orb.gd
@@ -48,6 +48,7 @@ func _physics_process(_delta: float) -> void:
 	
 func _on_body_entered(player):
 	if player.is_in_group("Player") and !used:
+		$sfx_pickup.play()
 		apply_effect(player)
 		used = true
 		used_timer.start()
