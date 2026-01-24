0f66b00b00dda95caeec08648920253616521fbf
vaoru <vaoru7@gmail.com>
2026-01-24 16:02:09 +0100

    main_menu_ost changed
    zamiast korzystac z playera w main menu to tworzymy go w globalsie Audio zeby nie bylo rwania podczas przejscia miedzy oknami

--- scenes/globals/audio.gd
+++ scenes/globals/audio.gd
@@ -4,3 +4,17 @@ func play_audio(audio):
 	if !audio.playing:
 		audio.pitch_scale = randf_range(0.7, 1.3)
 		audio.play()
+
+var main_menu_ost = AudioStreamPlayer.new()
+func _ready():
+	add_child(main_menu_ost)
+	main_menu_ost.stream = load("res://soundtrack/main_menu_normalized.ogg")
+	main_menu_ost.bus = "Master"
+
+func play_main_menu():
+	if not main_menu_ost.playing:
+		main_menu_ost.play()
+
+func stop_main_menu():
+	if main_menu_ost.playing:
+		main_menu_ost.stop()
