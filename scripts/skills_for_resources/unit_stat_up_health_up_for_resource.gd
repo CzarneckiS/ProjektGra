98b7a337c7326f3c144af7a9002b7195c8cb0496
vaoru <vaoru7@gmail.com>
2026-01-24 10:23:02 +0100

    nazwa -> Health Upgrade

--- scripts/skills_for_resources/unit_stat_up_health_up_for_resource.gd
+++ scripts/skills_for_resources/unit_stat_up_health_up_for_resource.gd
@@ -10,7 +10,7 @@ func use(unit: CharacterBody2D):
 	unit.max_health = unit.base_max_health + skill_level * 20
 	unit.health_bar.max_value = unit.max_health
 	unit.damage_bar.max_value = unit.max_health
-	unit.heal(20)
+	unit.heal(skill_level * 20)
 func _init() -> void:
 	unit_tags.append(Tags.UnitTag.ALLIED)
 	use_tags.append(Tags.UseTag.STAT_UP)
